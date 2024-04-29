7e8aca39d3bb5cc750ed2081ca6837ace7018640
LuckyLoo <101459565+LuckyLoo1730@users.noreply.github.com>
2024-04-29 19:15:57 +0100

    Uploaded the website

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const currentDate = new Date();
+const date = new Date();
+console.log(date);
+let time = document.querySelector(".time")
+function updateTime() {
+    const currentDate = new Date();
+    time.innerHTML = "📅 " + currentDate.toLocaleString();
+}
+updateTime();
+setInterval(updateTime, 1000)
+let changeTheme = document.querySelector(".theme2")
+changeTheme.addEventListener("click", function()
+{
+    document.body.classList.toggle("light")
+    changeTheme.innerHTML = "☀ Light"
+    document.querySelectorAll(".title, .time, .menu").forEach(element => {
+        element.classList.toggle("light")
+    })
+})
